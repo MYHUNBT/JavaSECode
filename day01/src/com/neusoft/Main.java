ac8eaa18d929c79d73c8b7a48d414c0b33405620
liuboting <532747090@qq.com>
2020-04-21 16:26:32 +0800

    第一次课

---
+++ day01/src/com/neusoft/Main.java
@@ -0,0 +1,30 @@
+package com.neusoft;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("刘柏廷");
+        //注释
+        //单行注释
+        /**
+         * 这是多行注释
+         * 在java中很常用
+         */
+        //了解常见的关键字 public，void，class
+        //保留字 const，goto
+        System.out.println();
+        /**1.声明变量并赋值
+         * 数据类型 变量名 = 变量值
+         * int a = 10;
+         * 2.先声明再赋值
+         * 数据类型 变量名
+         * int c;
+         * c = 100;
+         */
+        /**
+         * 声明变量并赋值
+         */
+
+    }
+}
---
+++ day01/src/com/neusoft/VariableDemo01.java
@@ -0,0 +1,41 @@
+package com.neusoft;
+
+/**
+ * @author Eric Lee
+ * @date 2020/4/6 16:43
+ */
+public class VariableDemo01 {
+    public static void main(String[] args) {
+
+        // byte类型
+        byte b = 20;
+        System.out.println(b);
+        // short类型
+        short s = 30;
+        System.out.println(s);
+        // int类型
+        int a = 100;
+        System.out.println(a);
+        // long类型
+        long lon = 100L;
+        System.out.println(lon);
+        // float 类型
+        float f = 10.4F;
+        System.out.println(f);
+        // double 类型
+        double d = 5.20;
+        System.out.println(d);
+
+        // char 类型
+        char c = 'A';
+        System.out.println(c);
+
+        // boolean类型
+        boolean bb = false;
+        System.out.println(bb);
+
+        // 建立  VariableDemo02
+        // 将以上变量改为先声明，后赋值的格式
+
+    }
+}
