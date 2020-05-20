7455e3e4f4b251cd365ebff6d6d8e571b85664b6
liuboting <532747090@qq.com>
2020-05-20 08:41:57 +0800

    static

---
+++ day06/src/com/neusoft/TestGame.java
@@ -0,0 +1,12 @@
+package com.neusoft;
+
+/**
+ * @author liuboting
+ * @date 2020/5/20 8:03
+ */
+
+public class TestGame {
+    public static void main(String[] args) {
+        Game game = new Game();
+    }
+}
