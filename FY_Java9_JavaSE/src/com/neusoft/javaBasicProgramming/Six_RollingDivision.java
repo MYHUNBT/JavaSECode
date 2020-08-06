6438d482952791e22589f149150bb7e1b9ca1c0e
liuboting <532747090@qq.com>
2020-08-06 10:42:19 +0800

    111

---
+++ FY_Java9_JavaSE/src/com/neusoft/javaBasicProgramming/Six_RollingDivision.java
@@ -0,0 +1,16 @@
+package com.neusoft.javaBasicProgramming;
+
+/**
+ * @author liuboting
+ * @date 2020/8/6 10:37
+ * 题目：输入两个正整数m和n，求其最大公约数和最小公倍数。
+ * 程序分析：利用辗除法。
+ */
+
+public class Six_RollingDivision {
+    public static void main(String[] args) {
+        int m = 56;
+        int n = 48;
+        
+    }
+}
